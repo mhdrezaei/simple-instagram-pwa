8b28b09e5d221b9ffeba979ef6f4c346f40fcaf9
رضائی - محمد <rezaee-moha@tehran.iri>
2022-12-17 18:02:55 +0330

    cache statit files for work offline

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,31 @@
-self.addEventListener('install' , function(event){
-    console.log('[service workwer] is installing... ' , event)
-})
-self.addEventListener('activate' , function(event){
-    console.log('[service workwer] is activating... ' , event);
-    return self.clients.claim();
-})
+self.addEventListener("install", function (event) {
+  console.log("[service workwer] is installing... ", event);
+  event.waitUntil(
+    caches.open("static").then(function (cache) {
+      console.log("start static chaches");
+      cache.addAll([
+        "/",
+        "/index.html",
+        "/src/js/app.js",
+        "/src/js/feed.js",
+        "/src/js/material.min.js",
+      ]);
+    })
+  );
+});
+self.addEventListener("activate", function (event) {
+  console.log("[service workwer] is activating... ", event);
+  return self.clients.claim();
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      if (response) {
+        return response;
+      } else {
+        return fetch(event.request);
+      }
+    })
+  );
+});
