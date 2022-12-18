0a5b20d87006bb7741fe09a015925fcf65f0a619
رضائی - محمد <rezaee-moha@tehran.iri>
2022-12-18 03:32:53 +0330

    error handeling(promise)

--- public/sw.js
+++ public/sw.js
@@ -35,6 +35,8 @@ self.addEventListener("fetch", function (event) {
                 cache.put(event.request.url , res.clone())
                 return res
             })
+        }).catch(function(err){
+
         })
       }
     })
